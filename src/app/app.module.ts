3b7672569d55c53f566f23e89c126f1e6d14cf39
Peter Burjan <peter.burjan@rbi.co.uk>
2017-03-26 11:40:37 +0100

    Added material and theme files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { PerfectScrollbarModule, PerfectScrollbarConfigInterface } from "angular2-perfect-scrollbar";
 
+import { RoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+const perfectScrollbarConfig: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    RoutingModule,
+    FlexLayoutModule,
+    PerfectScrollbarModule.forRoot(perfectScrollbarConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
